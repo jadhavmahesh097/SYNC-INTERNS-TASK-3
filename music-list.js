78454979dbb2843f6008e1d0948ae463ef2e78d7
Mahesh Jadhav <130351585+jadhavmahesh097@users.noreply.github.com>
2023-04-22 15:18:28 +0530

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,38 @@
+let allMusic = [
+  {
+    name: "Ghar Nahi Jana",
+    artist: "Zahrah Khan, Armaan Malik, Salma Agha",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Maan Meri Jaan",
+    artist: "King",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Kesariya",
+    artist: "Arijit Singh",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Har Har Shambhu Shiv Mahadeva",
+    artist: "Abhilipsa Panda, Jeetu Sharma,",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Kahani Suno",
+    artist: "	Kaifi Khalil",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "O Bedardeya",
+    artist: "Arijit Singh",
+    img: "music-6",
+    src: "music-6"
+  },
+];
